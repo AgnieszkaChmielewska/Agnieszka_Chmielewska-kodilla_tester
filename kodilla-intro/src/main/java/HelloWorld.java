783806f6aafd4c1059fd662f9b29f47aa0c25232
Agnieszka Chmielewska <chmielewska.agnieszka@tlen.pl>
2022-02-09 16:48:46 +0100

    Hello World

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main (String[] args){
-        System.out.println("Hellow Worls!");
+        System.out.println("Hellow World!");
         System.out.println("Agnieszka");
         System.out.println("18");
         System.out.println("C");
