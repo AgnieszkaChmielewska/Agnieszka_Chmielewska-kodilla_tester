8639055bfe62bedbe3a349765f13e304f9577799
Agnieszka Chmielewska <chmielewska.agnieszka@tlen.pl>
2022-02-15 15:47:30 +0100

    MainExtended and Main classes

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String [] args){
+        String myName="Agnieszka";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        String mySentence ="Hello from FirstClass";
+        System.out.println(carClass);
+        System.out.println(mySentence);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,13 @@
+public class MainExtended {
+    public static void main(String [] args){
+        String myName = "Agnieszka";
+        String country = "PL";
+        int yearOfBirth = 2000;
+        boolean areYouPolish = true;
+        double price = 3.14;
+        char group = 'A';
+        System.out.println( myName + ", " +country + ", year of birth: "+ yearOfBirth);
+        System.out.println( "Polish nationality: "+areYouPolish );
+        System.out.println(group +", "+price);
+    }
+}
