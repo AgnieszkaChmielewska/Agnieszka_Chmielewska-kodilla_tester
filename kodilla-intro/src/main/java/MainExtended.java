7fca60fe9ae607d9832da0ddb2f2b256f3d2bb76
Agnieszka Chmielewska <chmielewska.agnieszka@tlen.pl>
2022-02-17 16:10:04 +0100

    MainExtended

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -6,8 +6,18 @@ public class MainExtended {
         boolean areYouPolish = true;
         double price = 3.14;
         char group = 'A';
+        String why = sayHello();
         System.out.println( myName + ", " +country + ", year of birth: "+ yearOfBirth);
         System.out.println( "Polish nationality: "+areYouPolish );
         System.out.println(group +", "+price);
+        System.out.println(why);
+    }
+
+    private static String sayHello() {
+        String text = "Hello from FirstClass!";
+
+        return text;
     }
 }
+
+
