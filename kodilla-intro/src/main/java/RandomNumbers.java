8b1c6c1ae731b40d675e18378d0b51d5a540d128
Agnieszka Chmielewska <chmielewska.agnieszka@tlen.pl>
2022-03-14 18:49:29 +0100

    RandomNumbers and User changes

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,79 +1,50 @@
 import java.util.Random;
 
 public class RandomNumbers {
-    public static void main (String[] args) {
-        getSumOfRandomNumbers(50);
-        getTheSmallestRandomNumber(50);
-        getTheBiggestRandomNumber(50);
-        }
 
-    public static int getSumOfRandomNumbers(int max){
+    public static void main(String[] args) {
+    RandomNumbers r = new RandomNumbers();
+        r.getSumOfRandomNumbers(5000);
+        r.getTheSmallestRandomNumber(5000);
+        r.getTheBiggestRandomNumber(5000);
+    }
+
+    int max;
+    int theSmallestRandomNumber = max;
+    int theBiggestRandomNumber = 0;
+
+    public int getSumOfRandomNumbers(int max) {
         Random randomNumber = new Random();
         int result = 0;
         int sum = 0;
-        int theSmallestRandomNumber = max;
-        int theBiggestRandomNumber = 0;
+
 
         while (sum < max) {
             int a = randomNumber.nextInt(30);
-            System.out.println("Random number:"+a);
-            sum = sum+a;
-            System.out.println("Suma:" +sum);
+            System.out.println("Random number:" + a);
+            sum = sum + a;
+            System.out.println("Suma:" + sum);
 
-            if (a<theSmallestRandomNumber){
-            theSmallestRandomNumber = a;
+            if (a < theSmallestRandomNumber) {
+                theSmallestRandomNumber = a;
             }
 
-            System.out.println("The smallest random number was:"+theSmallestRandomNumber);
-            if (a>theBiggestRandomNumber){
+            System.out.println("The smallest random number was:" + theSmallestRandomNumber);
+            if (a > theBiggestRandomNumber) {
                 theBiggestRandomNumber = a;
             }
-            System.out.println("The biggest random number was:"+theBiggestRandomNumber);
+            System.out.println("The biggest random number was:" + theBiggestRandomNumber);
 
-            result ++;
-         }
+            result++;
+        }
         return result;
     }
 
-    public static int getTheSmallestRandomNumber(int max){
-        Random randomNumber = new Random();
-        int result = 0;
-        int sum = 0;
-        int theSmallestRandomNumber = max;
-
-        while (sum < max) {
-            int a = randomNumber.nextInt(30);
-            System.out.println("Random number:"+a);
-            sum = sum+a;
-            System.out.println("Suma:" +sum);
-            if (a<theSmallestRandomNumber){
-            theSmallestRandomNumber = a;
-            }
-            System.out.println("The smallest random number was:"+theSmallestRandomNumber);
-            result ++;
-        }
-        return theSmallestRandomNumber;
+    public int getTheSmallestRandomNumber(int max) {
+    return theSmallestRandomNumber;
     }
 
-    public static int getTheBiggestRandomNumber(int max){
-        Random randomNumber = new Random();
-        int result = 0;
-        int sum = 0;
-        int theBiggestRandomNumber = 0;
-
-        while (sum < max) {
-            int a = randomNumber.nextInt(30);
-            System.out.println("Random number:"+a);
-            sum = sum+a;
-            System.out.println("Suma:" +sum);
-
-            if (a>theBiggestRandomNumber){
-                theBiggestRandomNumber = a;
-            }
-            result ++;
-            System.out.println("The biggest random number was:"+theBiggestRandomNumber);
-         }
+    public int getTheBiggestRandomNumber(int max) {
         return theBiggestRandomNumber;
     }
-
- }
+}
--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -2,35 +2,36 @@ public class User {
     String name;
     int age;
 
-    public User ( String name, int age){
+    public User(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 
-    public static void main (String [] args){
-        User [] users = new User[8];
-        users [0] = new User("Jan", 39);
-        users [1] = new User("Ala", 59);
-        users [2] = new User("Aga", 19);
-        users [3] = new User("Olga", 30);
-        users [4] = new User("Asia", 19);
-        users [5] = new User("Ula", 9);
-        users [6] = new User("Ada", 3);
-        users [7] = new User("Ewa", 59);
+    public static void main(String[] args) {
+        User[] users = new User[8];
+        users[0] = new User("Jan", 39);
+        users[1] = new User("Ala", 59);
+        users[2] = new User("Aga", 19);
+        users[3] = new User("Olga", 30);
+        users[4] = new User("Asia", 19);
+        users[5] = new User("Ula", 9);
+        users[6] = new User("Ada", 3);
+        users[7] = new User("Ewa", 59);
 
         int sumOfUsersAge = 0;
-        for (int i =0;  i<users.length; i++ ) {
+        for (int i = 0; i < users.length; i++) {
             sumOfUsersAge = users[i].getAge() + sumOfUsersAge;
         }
-        System.out.println(sumOfUsersAge);
+//        System.out.println(sumOfUsersAge);
 
-         for (int i =0;  i<users.length; i++ ){
-             if(users[i].age> sumOfUsersAge/users.length)
-            System.out.println(users[i].name);
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].age < sumOfUsersAge / users.length) {
+                System.out.println(users[i].name);
+            }
         }
     }
 }
