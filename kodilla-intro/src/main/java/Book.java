6644fce8d2feed16cb9a02e5f2871e7cd43a32e8
Agnieszka Chmielewska <chmielewska.agnieszka@tlen.pl>
2022-03-14 20:10:55 +0100

    Book

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    public static void main(String[] args) {
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+    }
+
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public static Book of(String author, String title) {
+        Book book = new Book(author, title);
+        return book;
+    }
+
 }
