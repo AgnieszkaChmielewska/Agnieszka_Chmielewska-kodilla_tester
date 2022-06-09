16b2ec70ce2ee374004e5f42c381c8eb92e2774e
Agnieszka Chmielewska <chmielewska.agnieszka@tlen.pl>
2022-06-09 23:42:13 +0200

    14.3_Gherkin_wallet_improvements_2

--- kodilla-cucumber/src/test/java/wallet/PreventFromNegativeDepositsSteps.java
+++ kodilla-cucumber/src/test/java/wallet/PreventFromNegativeDepositsSteps.java
@@ -5,20 +5,17 @@ import org.junit.Assert;
 
 public class PreventFromNegativeDepositsSteps implements En {
 
-    private Wallet wallet = new Wallet();
+    final private Wallet wallet = new Wallet();
 
-    public void preventFromNegativeDeposits(){
-        When("I try to deposit {double}", (Double double1) -> {
-            wallet.deposit(-0.123456);
+    public void preventFromNegativeDepositsSteps() {
+
+        When("I try to deposit {int}", (Integer int1) -> {
+            wallet.deposit(-1);
         });
 
-        Then("nothing should be deposited", () -> {
-            Assert.assertEquals("Incorrect balance after negative deposit",200, wallet.getBalance());
-
-        });
-        When("I try to deposit {double}", (Double double1) -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new io.cucumber.java8.PendingException();
+        Then("{int} should be deposited", (Integer int1) -> {
+            wallet.deposit(0);
+            Assert.assertEquals(200, wallet.getBalance());
         });
 
     }
--- kodilla-cucumber/src/test/resources/wallet/prevent_from_negative_deposits.feature
+++ kodilla-cucumber/src/test/resources/wallet/prevent_from_negative_deposits.feature
@@ -1,6 +1,7 @@
 Feature: Prevent from negative deposits
+
   Scenario: User tries to deposit negative amount
     Given I have deposited $200 in my wallet
-    When I try to deposit -0.123456
-    Then nothing should be deposited
+    When I try to deposit -1
+    Then 0 should be deposited
     And The message should be displayed "Incorrect value. Please enter a valid amount"
